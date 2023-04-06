3c4b7e01827deaace0687504ed21215a48c57cc3
EwanP56 <120094397+EwanP56@users.noreply.github.com>
2023-04-06 10:53:20 +0200

    Add files via upload

---
+++ sae105_c11-master/script.js
@@ -0,0 +1,92 @@
+let i = 0;
+let j = 0;
+let tab = [i,j];
+
+function nextCar(nb) {
+  let width = window.innerWidth;
+
+  if (width > 550) {
+
+    if (tab[nb] == 0) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 1))';
+      tab[nb]++;
+    } else if(tab[nb] == 1) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 2))';
+      tab[nb]++;
+    }else if(tab[nb] == 2) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 3))';
+      tab[nb]++;
+    }else if(tab[nb] == 3) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 4))';
+      tab[nb]++;
+    }else if(tab[nb] == 4) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 5))';
+      tab[nb]++;
+    }
+  }else {
+    if (tab[nb] == 0) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 1))';
+      tab[nb]++;
+    } else if(tab[nb] == 1) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 2))';
+      tab[nb]++;
+    }else if(tab[nb] == 2) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 3))';
+      tab[nb]++;
+    }else if(tab[nb] == 3) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 4))';
+      tab[nb]++;
+    }else if(tab[nb] == 4) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 5))';
+      tab[nb]++;
+    }
+  }
+};
+
+function prevCar(nb) {
+  let width = window.innerWidth;
+
+  if (width > 550) {
+
+    if (tab[nb] == 2) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 1))';
+      tab[nb]--;
+    } else if(tab[nb] == 1) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(0)';
+      tab[nb]--;
+    }else if(tab[nb] == 3) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 2))';
+      tab[nb]--;
+    }else if(tab[nb] == 4) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 3))';
+      tab[nb]--;
+    }else if(tab[nb] == 5) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-550px * 4))';
+      tab[nb]--;
+    }
+  }else {
+    if (tab[nb] == 2) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 1))';
+      tab[nb]--;
+    } else if(tab[nb] == 1) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(0)';
+      tab[nb]--;
+    }else if(tab[nb] == 3) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 2))';
+      tab[nb]--;
+    }else if(tab[nb] == 4) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 3))';
+      tab[nb]--;
+    }else if(tab[nb] == 5) {
+      document.querySelectorAll('.slides')[nb].style.transform = 'translateX(calc(-480px * 4))';
+      tab[nb]--;
+    }
+  }
+};
+
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector('body');
+
+toggle.addEventListener('click', function() {
+    body.classList.toggle('open');
+})
